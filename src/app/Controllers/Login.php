defdef70aa195f92707171974931f8b7adf31d88
pvillaroel <pvillaroel@tripadvisor.com>
2021-12-06 06:41:54 -0500

    adding more documentation on controller methods

--- src/app/Controllers/Login.php
+++ src/app/Controllers/Login.php
@@ -18,19 +18,27 @@ class Login extends BaseController
 		$this->login();
 	}
 
+	/**
+	 * Loads the login page
+	 */
 	public function login(){
 		echo view('templates/header');
 		echo view('login');
 		echo view('templates/footer');
 	}
-	
+	/**
+	 * Loads all the existing products
+	 * Doesn't require authentication
+	 */
 	public function products(){
 		$data['all_products'] = $this->productModel->getAvailableProducts();
 		echo view('templates/header');
 		echo view('products', $data);
 		echo view('templates/footer');
 	}
-
+	/**
+	 * Logout method that ends session and reloads login page
+	 */
 	public function logout(){
 		$this->session->set(['logged_in' => false]);
 		$this->session->destroy();
